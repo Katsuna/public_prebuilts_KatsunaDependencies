f0d0c9e551bb067b3d971db790ca9da789410fe7
George G <kreach3r@users.noreply.github.com>
2017-02-24 18:16:02 +0100

    initial commit

---
+++ dependencies.mk
@@ -0,0 +1,6 @@
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += jodatime:libs/jodatime-2.9.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += picasso:libs/picasso-2.5.2.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:libs/roundedimageview-2.2.1.aar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += fabtransformation:libs/fabtransformation-1.0.0.aar
