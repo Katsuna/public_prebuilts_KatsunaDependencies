ef99fdd4985e2860f573d948377c67e8fb7364eb
George G <kreach3r@gmail.com>
2018-08-01 14:30:48 +0300

    Android.mk: comments

--- Android.mk
+++ Android.mk
@@ -1,12 +1,27 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+# Android SDK
+
+
+# third party
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += jodatime:libs/jodatime-2.9.2.jar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += picasso:libs/picasso-2.5.2.jar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += roundedimageview:libs/roundedimageview-2.2.1.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += fabtransformation:libs/fabtransformation-1.0.0.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += ezvcard:libs/ezvcard-0.9.9.jar
 
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androidvolley:libs/androidvolley-1.0.19.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += googlegson:libs/googlegson-2.6.2.jar
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += dashclock:libs/dashclock-2.0.0.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += greendao:libs/greendao-3.2.0.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += williamchart:libs/williamchart-2.2.aar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += bottombar:libs/bottombar-1.4.0.1.aar
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androiddevicenames:libs/androiddevicenames-1.1.3.aar
+
+# Google Play Services
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += playbase920:libs/play-services-base-9.2.0.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += playbasement920:libs/play-services-basement-9.2.0.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += playcontextmanager920:libs/play-services-contextmanager-9.2.0.jar
@@ -27,15 +42,5 @@ LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += firebasecrash1160:libs/firebase-crash-11
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += firebasecrashlicense1160:libs/firebase-crash-license-11.6.0.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += firebaseiid1160:libs/firebase-iid-11.6.0.aar
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += firebaseiidlicense1160:libs/firebase-iid-license-11.6.0.aar
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androidvolley:libs/androidvolley-1.0.19.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += googlegson:libs/googlegson-2.6.2.jar
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += dashclock:libs/dashclock-2.0.0.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += greendao:libs/greendao-3.2.0.jar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += williamchart:libs/williamchart-2.2.aar
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += bottombar:libs/bottombar-1.4.0.1.aar
-
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES += androiddevicenames:libs/androiddevicenames-1.1.3.aar
 
 include $(BUILD_MULTI_PREBUILT)
